e1dd8009c6222170999d8bc615e3885aed5e49d4
Simin Salimi <simsim.salimi@gmail.com>
2021-03-24 23:16:33 -0400

    first app
    push to github from project

--- src/container/App.js
+++ src/container/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from '../image/logo.svg';
+import Sidebar from '../sidebar/Sidebar';
 import './App.css';
 
 function App() {
@@ -7,17 +8,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={ssLogo} className="SS-logo" alt="logo" /> */}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome To My Little World.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Sidebar/>
       </header>
     </div>
   );
---
+++ src/sidebar/Sidebar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Sidebar.css';
+
+function Sidebar() {
+  return (
+    <div className="Sidebar">
+     <div id="mySidebar" class="sidebar">
+  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+  <a href="#">About</a>
+  <a href="#">Services</a>
+  <a href="#">Clients</a>
+  <a href="#">Contact</a>
+</div>
+
+<div id="main">
+  <button class="openbtn" onclick="openNav()">&#9776; Open Sidebar</button>
+  <h2>Collapsed Sidebar</h2>
+  <p>Content...</p>
+</div>
+    </div>
+  );
+}
+
+export default Sidebar;
